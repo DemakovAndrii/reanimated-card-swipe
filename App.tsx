7f0e748d3d7f2acc63506ba55bbe86fdbb7ebf2c
DemakovAndrii <demakov1155@gmail.com>
2023-02-18 14:21:37 +0200

    2

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import SliderComp from './src/comp/SliderComp';
+import SliderCompMap from './src/comp/SliderCompMap';
 
 function App() {
   return (
     <GestureHandlerRootView>
       <SafeAreaView>
-        <SliderComp />
+        {/* <SliderComp /> */}
+        <SliderCompMap />
       </SafeAreaView>
     </GestureHandlerRootView>
   );
